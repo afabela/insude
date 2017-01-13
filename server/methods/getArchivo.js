8c8acd4202b53b5fd6ddf0bf43fca923f7daa8bc
Alfonso Duarte <alfonsoduarte@gmail.com>
2017-01-12 20:09:42 -0700

    Gafetes

--- server/methods/getArchivo.js
+++ server/methods/getArchivo.js
@@ -1,16 +1,15 @@
 
 Meteor.methods({
   
-  
-  getGafetes: function (participantes) {
+  getGafetes: function (participantes) { 
 		
-		var fs = Npm.require('fs');
-    var Docxtemplater = Npm.require('docxtemplater');
-		var JSZip = Npm.require('jszip');
-		var ImageModule = Npm.require('docxtemplater-image-module')
-		//var unoconv = require('unoconv');
+		var fs = require('fs');
+    var Docxtemplater = require('docxtemplater');
+		var JSZip = require('jszip');
+		var ImageModule = require('docxtemplater-image-module')
 		
-	
+	  //var meteor_root = require('fs').realpathSync( process.cwd() + '/../' );
+		//console.log(meteor_root);
 		
 		var opts = {}
 			opts.centered = false;
@@ -20,59 +19,54 @@ Meteor.methods({
 		}
 		
 		opts.getSize=function(img,tagValue, tagName) {
-		    return [80,80];
+		    return [100,100];
 		}
 		
 		var imageModule=new ImageModule(opts);
 		
-		
-		
-		//_.each(participantes, function(participante){
-										
+		_.each(participantes, function(participante){
+				if (participante.foto != "")
+				{											
+					var f = String(participante.foto);
+					participante.foto = f.replace('data:image/jpeg;base64,', '');
+					
 					// create buffer object from base64 encoded string, it is important to tell the constructor that the string is base64 encoded
-		//	    var bitmap = new Buffer(participante.foto, 'base64');
-			    // write buffer to file
-		//	    fs.writeFileSync(process.cwd()+"/fotos/"+participante.curp+".png", bitmap);
-					//participante.foto = process.cwd() + "/fotos/"+participante.curp+".png";
-		//			participante.foto = "foto.png";
-				
-		//})
+			    var bitmap = new Buffer(participante.foto, 'base64');
+			    // write buffer to file					
+					
+					//Usando Meteor_root
+					fs.writeFileSync("/home/insude/archivos/"+participante.curp+".png", bitmap);
+					participante.foto = "/home/insude/archivos/"+participante.curp+".png";
+					
+				}
+		})
+
 		
-				
 		var content = fs
     							//.readFileSync("/Users/alfonsoduarte/Documents/Meteor/deporteb/cedula.docx", "binary");
-    							.readFileSync(process.cwd()+"/app/server/cedula.docx", "binary");
+    							.readFileSync("/home/insude/archivos/gafete.docx", "binary");
 	  
 		var zip = new JSZip(content);
 		var doc=new Docxtemplater()
 								.attachModule(imageModule)
 								.loadZip(zip)
 		
-		doc.setOptions({
-		    parser: function(tag) {
-		      return {
-		        'get': function(scope) {
-		          console.log('evaluating', tag, 'in', JSON.stringify(scope));
-		          console.log('evaluated to', scope[tag]);
-		          if (tag === '.') {
-		            return scope;
-		          } else {
-		            return scope[tag];
-		          }
-		        }
-		      };
-		    },
-		})
-		
-		
+		doc.setData({participantes})
 		
 		doc.render();
  
 		var buf = doc.getZip()
              		 .generate({type:"nodebuffer"});
  
-		fs.writeFileSync(process.cwd()+"/app/server/ceulaSalida.docx",buf);
+		fs.writeFileSync("/home/insude/archivos/gafeteSalida.docx",buf);
 		
+		
+		//Pasar a base64
+		// read binary data
+    var bitmap = fs.readFileSync("/home/insude/archivos/gafeteSalida.docx");
+    
+    // convert binary data to base64 encoded string
+    return new Buffer(bitmap).toString('base64');
 		
 		
 		
@@ -81,9 +75,9 @@ Meteor.methods({
 		
 		var fs = Npm.require('fs');
     
-		var JSZip = Npm.require('jszip');
-		var Docxtemplater = Npm.require('docxtemplater');
-		var ImageModule = Npm.require('docxtemplater-image-module');
+		var JSZip = require('jszip');
+		var Docxtemplater = require('docxtemplater');
+		var ImageModule = require('docxtemplater-image-module');
 		//var unoconv = Npm.require('unoconv');
 		
 		var meteor_root = Npm.require('fs').realpathSync( process.cwd() + '/../' );
